a21a78defddfac2ec76667993410b840ac29f40e
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 02:07:29 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,7 @@
 require_relative "./vehicle.rb"
 class Car < Vehicle
 
-  def go
+  def go #did we just change the def go to something in this car class?
       "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
 
